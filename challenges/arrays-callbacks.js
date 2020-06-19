de1af7f67c2eb437a781d6aae3652277e49b4090
Harry Henry Gebel <hhgebel@gmail.com>
2020-06-19 11:52:16 -0400

    array-callbacks.js request 1

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -21,6 +21,8 @@ The zoos want to display both the scientific name and the animal name in front o
 
 */
 const displayNames = [];
+zooAnimals.forEach(animal => displayNames.push(
+  `Name: ${animal.name}, Scientific: ${animal.scientific_name}`));
 console.log(displayNames);
 
 /* Request 2: .map()
