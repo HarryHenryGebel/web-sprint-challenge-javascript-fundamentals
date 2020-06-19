018ce52d03a2216c6e982902e6a0febf1d01252f
Harry Henry Gebel <hhgebel@gmail.com>
2020-06-19 11:58:19 -0400

    array-callbacks.js request 2

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -27,11 +27,12 @@ console.log(displayNames);
 
 /* Request 2: .map()
 
-The zoos need a list of all their animal's names (animal_name only) converted to lower case. Using map, create a new array of strings named lowCaseAnimalNames, each string following this pattern: "jackal, asiatic". Log the resut.
+The zoos need a list of all their animal's names (animal_name only) converted to lower case. Using map, create a new array of strings named lowCaseAnimalNames, each string following this pattern: "jackal, asiatic". Log the result.
 
 */
 
-const lowCaseAnimalNames
+const lowCaseAnimalNames = zooAnimals.map(
+  animal => animal.animal_name.toLowerCase());
 console.log(lowCaseAnimalNames);
 
 /* Request 3: .filter()
